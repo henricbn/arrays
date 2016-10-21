7cbf79b49eedd5eb4badc19d986d0f01f29bec5e
Henric Bernhardsson <henrictrc@hotmail.com>
2016-10-21 12:07:37 +0200

    upp

--- array/Program.cs
+++ array/Program.cs
@@ -10,6 +10,32 @@ namespace array
     {
         static void Main(string[] args)
         {
+            /* string[] namn = new string[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("mata in ett namn:");
+                namn[i] = Console.ReadLine();
+                
+            }
+            for(int i = 0; i<10; i++)
+            {
+                Console.WriteLine(namn[i]);
+            }
+            */
+            /* övning2
+            int[] tal = new int[10];
+            for(int i = 0; i<10; i++)
+            {
+                Console.Write("Skriv ett tal:");
+                tal[i] = int.Parse(Console.ReadLine());
+                }
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(tal[i]);
+            } */
+            int[] numbArray = { 12, 5, 6, 1, 2, 3, -2, -7, 0 };
+
+
         }
     }
 }
