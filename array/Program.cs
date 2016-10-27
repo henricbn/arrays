04305f3dd74f104f25c301ff78558a03867f448d
Henric Bernhardsson <henrictrc@hotmail.com>
2016-10-27 13:09:09 +0200

    övning 3 uppe

--- array/Program.cs
+++ array/Program.cs
@@ -34,6 +34,20 @@ namespace array
                 Console.WriteLine(tal[i]);
             } */
             int[] numbArray = { 12, 5, 6, 1, 2, 3, -2, -7, 0 };
+            int numero = 0;
+            for (int i = 0; i < numbArray.Length; i++)
+            {
+                for(int q = i + 1; q < numbArray.Length; q++)
+                {
+                    if ( numbArray[i] > numbArray[q])
+                    {
+                        numero = numbArray[q];
+                        numbArray[q] = numbArray[i];
+                        numbArray[i] = numero;
+                    }
+                }
+                Console.WriteLine(numbArray[i]);
+            } 
 
 
         }
